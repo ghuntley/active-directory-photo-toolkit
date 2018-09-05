c85910d8632622e21241615ab397ccb5cc8aed4a
CaptainQwerty <antonybragg@gmail.com>
2018-09-05 22:13:26 +0100

    Created Thumbnail Interface

---
+++ src/ActiveDirectoryPhotoToolkit/IThumbnail.cs
@@ -0,0 +1,9 @@
+﻿namespace ActiveDirectoryPhotoToolkit
+{
+    internal interface IThumbnail
+    {
+        string Name { get; set; }
+        byte[] ThumbnailData { get; set; }
+        ActiveDirectoryPhoto.Format Format { get; set; }
+    }
+}
